25ba5cef259b0c803b03f970ed9211fa17786927
timurseta <timur_seta@hotmail.com>
2021-09-17 18:11:51 +0200

    lengit of a string

---
+++ ex08/src/index.js
@@ -0,0 +1,13 @@
+function main () {
+    // Only change code below this line
+
+    var lastName = "Doe";
+    var lastNameLenght = lastName.length;
+
+
+
+    // Only change code above this line
+    return lastNameLenght;
+}
+console.log(main());
+module.exports = main;
