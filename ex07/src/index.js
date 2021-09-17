8c33dae94ac2db7b0b54b3a0319724194546eab1
timurseta <timur_seta@hotmail.com>
2021-09-17 17:40:41 +0200

    appending variable to string

---
+++ ex07/src/index.js
@@ -0,0 +1,15 @@
+function main () {
+    // Only change code below this line
+
+    var myString = "Mistral is ";
+    var someAdjective = "the best!";
+
+    myString += someAdjective;
+
+
+
+    // Only change code above this line
+    return myString;
+}
+console.log(main());
+module.exports = main;
