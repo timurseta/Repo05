aa7829808f024b63120dbb5736f3630506065c7f
timurseta <timur_seta@hotmail.com>
2021-09-18 09:15:31 +0200

    edited code

--- ex10/src/index.js
+++ ex10/src/index.js
@@ -1,8 +1,8 @@
 function main () {
     // Only change code below this line
 
-    var myString = "Jello World"
-    myString = "Hello World"
+    var myString = "Jello World";
+    myString = "Hello World";
 
     // Only change code above this line
     return myString;
